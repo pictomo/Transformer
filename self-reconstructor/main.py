3b7bb6f7f44d7577fe40574309a23d5ff9d63493
pictomo <tomo.pict@gmail.com>
2024-12-25 00:02:25 +0900

    [update]no more read-open

--- self-reconstructor/main.py
+++ self-reconstructor/main.py
@@ -3,8 +3,18 @@
 import sys
 
 path = sys.argv[0]
-# overwrite the file with its own content
-with open(path, "r") as file:
-    content = file.read()
+
+content = """#!/usr/bin/env python3
+
+import sys
+
+path = sys.argv[0]
+
+content = {0}{1}{0}
+
 with open(path, "w") as file:
-    file.write(content)
+    file.write(content.format(chr(34) + chr(34) + chr(34), content))
+"""
+
+with open(path, "w") as file:
+    file.write(content.format(chr(34) + chr(34) + chr(34), content))
